82487b93176455847b60b590475f29f27138913f
김국진 <rnrwls0211@naver.com>
2022-08-01 17:21:12 +0900

    반 디테일 보기 컴포넌트 추가

--- front/ddingddong/src/pages/Master/ManageGroup/MasterManageGroup.jsx
+++ front/ddingddong/src/pages/Master/ManageGroup/MasterManageGroup.jsx
@@ -2,13 +2,26 @@
 import GroupManagement from '../../../components/Group/GroupManagement';
 import RegistMember from '../../../components/Member/RegistMember';
 import { Box } from '@mui/material';
+import { getApiData, postApiData } from '../../../api/axios';
+import { useState, useEffect } from 'react';
 
 const MasterManageGroup = () => {
+  const [server, setServer] = useState([]);
+
+  // useEffect 함수 내에서 async await 동작하는 함수 생성 후, 바로 호출
+  useEffect(() => {
+    const getApi = async () => {
+      const data = await postApiData('');
+      setServer((server) => data);
+      console.log('데이터:', server);
+    };
+    getApi('');
+  }, []);
+
   return (
     <Box style={{ borderRadius: '20px', background: 'white' }}>
       <GroupManagement />
       <Box style={{ width: '100%', marginTop: '10px', marginBottom: '10px' }} />
-      \
     </Box>
   );
 };
