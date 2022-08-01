0b054fd5be51d76acbfc1dfbf97d175bf7d5417e
이소영 <sodud0695@naver.com>
2022-08-01 12:09:06 +0900

    [S07P12E102-72] feat:back: 반 라우터 등록

---
+++ back/app/controllers/class.js
@@ -0,0 +1,36 @@
+const db = require("../models");
+const Class = db.class;
+
+// 반 등록
+// [post] /class/register
+exports.class_regist = async function (req, res, next) {
+
+}
+
+
+// 반 목록 조회
+// [get]  /class/list
+exports.class_list = async function (req, res) { 
+
+}
+
+
+// 반 상세 조회
+// [get]  /class/:class_no
+exports.class_detail = async function (req, res) { 
+
+}
+
+
+// 반 정보 수정
+// [put]  /class/:class_no
+exports.class_update = async function (req, res) { 
+
+}
+
+
+// 반 정보 삭제
+// [delete] /class/:class_no
+exports.class_remove = async function (req, res) { 
+
+}
