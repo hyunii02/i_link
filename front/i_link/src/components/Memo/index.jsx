4f219d1faa67c6d9f5b79b29d9b05d6fadfdd7f3
배지우 <wldn102@naver.com>
2022-08-02 17:25:02 +0900

    [S07P12E102-171] feat:front 알림장 컴포넌트 진행

--- front/i_link/src/components/Memo/index.jsx
+++ front/i_link/src/components/Memo/index.jsx
@@ -1,36 +1,59 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
 
-export default function Memo() {
+
+const cards = [{cards_id: 1 ,cards_title: "8/1" , cards_content:"방가방가",},
+{cards_id: 2 ,cards_title: "7/30", cards_content:"포켓몬",},
+{cards_id: 3 ,cards_title: "7/20", cards_content:"안녕",},
+{cards_id: 4 ,cards_title: "7/15",  cards_content:"반가워",},
+];
+
+const theme = createTheme();
+
+export default function Album() {
+  
   return (
-    <Card sx={{ maxWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <main>
+        {/* Hero unit */}
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: 300, display: 'flex', flexDirection: 'column' }}
+                >
+                  
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.cards_title}
+                    </Typography>
+                    <Typography>
+                      {card.cards_content}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </ThemeProvider>
   );
 }
--- front/i_link/src/pages/Master/Memo/MasterMemo.jsx
+++ front/i_link/src/pages/Master/Memo/MasterMemo.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Album from '../../../components/Memo';
+import CreateMemo from '../../../components/Memo/creatememo';
 
 const MasterMemo = () => {
-  return <div>MasterMemo</div>;
+  return <div><CreateMemo/></div>;
 };
 
 export default MasterMemo;
