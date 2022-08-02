f1def41d711f67e9ba06bb4953a5a8c96a4b2c8d
강민재 <kmj5052@gmail.com>
2022-08-02 16:33:48 +0900

    [S07P12E102-148] feat:front: 전역 상태 작업

--- front/i_link/src/App.js
+++ front/i_link/src/App.js
@@ -29,46 +29,49 @@ import TeacherNotice from './pages/Teacher/Notice/TeacherNotice';
 import TeacherQuiz from './pages/Teacher/Quiz/TeacherQuiz';
 import Error from './pages/Error/Error';
 import LayoutWithoutSidebar from './layout/WithoutSidebar';
+import { UserProvider } from './context/user';
 
 const App = () => {
   return (
-    <Routes>
-      {/* 사이드바 없는 화면(로그인, 회원가입, 회원정보수정) */}
-      <Route element={<LayoutWithoutSidebar />}>
-        <Route path="/" element={<UserLogin />} />
-        <Route path="/user/signup" element={<UserSignUp />} />
-        <Route path="/user/update" element={<UserUpdate />} />
-      </Route>
-      {/* 원장님, 학부모, 선생님 */}
-      <Route element={<LayOut />}>
-        {/* 원장님 */}
-        <Route path="/master/diet" element={<MasterDiet />} />
-        <Route path="/master/managegroup" element={<MasterManageGroup />} />
-        <Route path="/master/managemember" element={<MasterManageMember />} />
-        <Route path="/master/memo" element={<MasterMemo />} />
-        <Route path="/master/notice" element={<MasterNotice />} />
-        <Route path="/master/registacademy" element={<MasterRegistAcademy />} />
-        {/* 학부모 */}
-        <Route path="/parents/diet" element={<ParentsDiet />} />
-        <Route path="/parents/home" element={<ParentsHome />} />
-        <Route path="/parents/notice" element={<ParentsNotice />} />
-        <Route path="/parents/quiz" element={<ParentsQuiz />} />
-        <Route path="/parents/registkid" element={<ParentsRegistKid />} />
-        {/* 선생님 */}
-        <Route path="/teacher/detail" element={<TeacherDetail />} />
-        <Route path="/teacher/diet" element={<TeacherDiet />} />
-        <Route path="/teacher/management" element={<TeacherManagement />} />
-        <Route path="/teacher/memo" element={<TeacherMemo />} />
-        <Route path="/teacher/notice" element={<TeacherNotice />} />
-        <Route path="/teacher/quiz" element={<TeacherQuiz />} />
-      </Route>
-      {/* 헤더, 사이드바 없는 화면 */}
-      <Route path="*" element={<Error />} />
-      <Route path="/kiosk/award" element={<KioskAward />} />
-      <Route path="/kiosk/main" element={<KioskMain />} />
-      <Route path="/kiosk/quiz" element={<KioskQuiz />} />
-      <Route path="/kiosk/vote" element={<KioskVote />} />
-    </Routes>
+    <UserProvider>
+      <Routes>
+        {/* 사이드바 없는 화면(로그인, 회원가입, 회원정보수정) */}
+        <Route element={<LayoutWithoutSidebar />}>
+          <Route path="/" element={<UserLogin />} />
+          <Route path="/user/signup" element={<UserSignUp />} />
+          <Route path="/user/update" element={<UserUpdate />} />
+        </Route>
+        {/* 원장님, 학부모, 선생님 */}
+        <Route element={<LayOut />}>
+          {/* 원장님 */}
+          <Route path="/master/diet" element={<MasterDiet />} />
+          <Route path="/master/managegroup" element={<MasterManageGroup />} />
+          <Route path="/master/managemember" element={<MasterManageMember />} />
+          <Route path="/master/memo" element={<MasterMemo />} />
+          <Route path="/master/notice" element={<MasterNotice />} />
+          <Route path="/master/registacademy" element={<MasterRegistAcademy />} />
+          {/* 학부모 */}
+          <Route path="/parents/diet" element={<ParentsDiet />} />
+          <Route path="/parents/home" element={<ParentsHome />} />
+          <Route path="/parents/notice" element={<ParentsNotice />} />
+          <Route path="/parents/quiz" element={<ParentsQuiz />} />
+          <Route path="/parents/registkid" element={<ParentsRegistKid />} />
+          {/* 선생님 */}
+          <Route path="/teacher/detail" element={<TeacherDetail />} />
+          <Route path="/teacher/diet" element={<TeacherDiet />} />
+          <Route path="/teacher/management" element={<TeacherManagement />} />
+          <Route path="/teacher/memo" element={<TeacherMemo />} />
+          <Route path="/teacher/notice" element={<TeacherNotice />} />
+          <Route path="/teacher/quiz" element={<TeacherQuiz />} />
+        </Route>
+        {/* 헤더, 사이드바 없는 화면 */}
+        <Route path="*" element={<Error />} />
+        <Route path="/kiosk/award" element={<KioskAward />} />
+        <Route path="/kiosk/main" element={<KioskMain />} />
+        <Route path="/kiosk/quiz" element={<KioskQuiz />} />
+        <Route path="/kiosk/vote" element={<KioskVote />} />
+      </Routes>
+    </UserProvider>
   );
 };
 
