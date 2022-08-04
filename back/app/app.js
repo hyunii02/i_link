29b23dfd03b9552fff64c30e1c7f9b66d32a5d21
soo1095 <soo1095@naver.com>
2022-08-05 00:30:48 +0900

    fix:back: Local용 https setting 추가

--- back/app/app.js
+++ back/app/app.js
@@ -12,9 +12,16 @@ const session = require("express-session");
 const https = require("https");
 const fs = require("fs");
 
-var privateKey = fs.readFileSync("/etc/letsencrypt/live/i7e102.p.ssafy.io/privkey.pem");
-var certificate = fs.readFileSync("/etc/letsencrypt/live/i7e102.p.ssafy.io/cert.pem");
-var ca = fs.readFileSync("/etc/letsencrypt/live/i7e102.p.ssafy.io/fullchain.pem");
+// Local 용
+var privateKey = fs.readFileSync("pem/privkey.pem");
+var certificate = fs.readFileSync("pem/cert.pem");
+var ca = fs.readFileSync("pem/fullchain.pem");
+
+// Service 용
+// var privateKey = fs.readFileSync("/etc/letsencrypt/live/i7e102.p.ssafy.io/privkey.pem");
+// var certificate = fs.readFileSync("/etc/letsencrypt/live/i7e102.p.ssafy.io/cert.pem");
+// var ca = fs.readFileSync("/etc/letsencrypt/live/i7e102.p.ssafy.io/fullchain.pem");
+
 const credentials = { key: privateKey, cert: certificate, ca: ca };
 
 // Routes
