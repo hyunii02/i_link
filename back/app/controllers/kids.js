635a0db238ddf8fac47240139d8a58623d321d20
soaeng <sodud0695@naver.com>
2022-08-05 01:31:30 +0900

    [S07P12E102-64] feat:back: 자녀 등록

--- back/app/controllers/kids.js
+++ back/app/controllers/kids.js
@@ -5,7 +5,31 @@ const db = require(path.join(__dirname, "..", "models"));
 const Kids = db.kids;
 
 // 자녀 등록
-exports.kid_regist = function (req, res) {};
+// [post] /kids/register
+exports.kid_regist = async function (req, res) {
+  // *** Content-Type: application/json
+  // 아이
+  const kid = {
+    kid_name: req.body.kidName,
+    kid_birth: req.body.kidBirth ? req.body.kidBirth : null,
+    kid_gender: req.body.kidGender ? req.body.kidGender : null,
+    kid_profile_url: req.body.kidProfileUrl ? req.body.kidProfileUrl : null,
+    parents_no: req.body.userNo, // front에서 input type: hidden
+  };
+
+  await Kids.create(kid)
+    .then((data) => {
+      console.log("자녀 등록 완료", data.dataValues);
+      res.status(200).json({
+        message: "자녀 등록 완료",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: err.message || "자녀 등록 실패",
+      });
+    });
+};
 
 // 자녀 유치원 등록
 exports.kid_center_regist = function (req, res) {};
