e1208df6e57fe8d55269b6e0b68c3a7463dee1bf
강민재 <kmj5052@gmail.com>
2022-08-05 11:28:56 +0900

    fix/ api axios.jsx 파일 수정

--- front/i_link/src/api/axios.jsx
+++ front/i_link/src/api/axios.jsx
@@ -2,32 +2,6 @@
 
 const baseURL = "http://i7e102.p.ssafy.io:8000";
 
-// GET 방식으로 Server API에 요청해서 데이터를 받아들임.
-// param[0] : 위 SERVER_URL 이후의 주소를 입력
-const getApiData = async (url, path_params) => {
-  let URL = baseURL + url;
-  if (path_params) {
-    URL += path_params;
-  }
-  const response = await axios.get(URL);
-  console.log("response:", response);
-  return response.data;
-};
-
-// POST 방식으로 Server API에 데이터를 body에 전송
-// param[0] :
-// param[1] :
-const postApiData = async (url, path_params, body) => {
-  let URL = baseURL + url;
-  if (path_params) {
-    URL += path_params;
-  }
-
-  const response = await axios.post(URL, body);
-  console.log("response:", response);
-  return response.data;
-};
-
 const urls = {
   fetchLogin: "/users/login",
   fetchLogout: "/users/logout",
