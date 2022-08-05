8b51d420bd5018e07152d05685319055a9062f38
강민재 <kmj5052@gmail.com>
2022-08-05 11:38:35 +0900

    fix/ axios.jsx수정

--- front/i_link/src/pages/Master/ManageGroup/MasterManageGroup.jsx
+++ front/i_link/src/pages/Master/ManageGroup/MasterManageGroup.jsx
@@ -2,35 +2,11 @@
 import GroupManagement from "../../../components/Group/GroupManagement";
 import RegistKids from "../../../components/Kids/RegistKids";
 import { Box } from "@mui/material";
-import { getApiData, postApiData } from "../../../api/axios";
 import { useState, useEffect, React } from "react";
 
 const MasterManageGroup = () => {
   const [server, setServer] = useState([]);
 
-  // useEffect 함수 내에서 async await 동작하는 함수 생성 후, 바로 호출
-<<<<<<< HEAD
-  /*
-  useEffect(() => {
-    const getApi = async () => {
-      const data = await postApiData("");
-      setServer((server) => data);
-      console.log("데이터:", server);
-    };
-    getApi("");
-  }, []);
-*/
-=======
-  // useEffect(() => {
-  //   const getApi = async () => {
-  //     const data = await postApiData("");
-  //     setServer((server) => data);
-  //     console.log("데이터:", server);
-  //   };
-  //   getApi("");
-  // }, []);
-
->>>>>>> b92ca108cb20246c3af9ee850a3293f2d417b8cd
   return (
     <Box>
       <GroupManagement />
