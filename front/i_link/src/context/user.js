4a7dfd67c3104923ab9bf9181dde0b3a2058680e
강민재 <kmj5052@gmail.com>
2022-08-07 23:32:05 +0900

    [S07P12E102-148] feat:front:전역 상태에 유저번호, 유저전화번호, 유저센터번호, 유저그룹번호 추가

--- front/i_link/src/context/user.js
+++ front/i_link/src/context/user.js
@@ -6,8 +6,11 @@ const UserContext = createContext(null);
 const UserProvider = ({children}) => {
   // 세션에 저장된 데이터가 있다면 가져옴 
   const [userName, setUserName] = useState(()=>sessionStorage.getItem('userName'))
-
   const [userType, setUserType] = useState(()=>sessionStorage.getItem('userType'))
+  const [userNo, setUserNo] = useState(()=>sessionStorage.getItem('userNo'))
+  const [userPhone, setUserPhone] = useState(()=>sessionStorage.getItem('userPhone'))
+  const [userGroup, setUserGroup] = useState(()=>sessionStorage.getItem('userGroup'))
+  const [userCenter, setUserCenter] = useState(()=>sessionStorage.getItem('userCenter'))
 
   // 값이 변경되면 세션에 적용
   useEffect(()=>{
@@ -17,9 +20,36 @@ const UserProvider = ({children}) => {
   useEffect(()=>{
     sessionStorage.setItem('userType',userType)
   },[userType])
+  
+  useEffect(()=>{
+    sessionStorage.setItem('userNo',userNo)
+  },[userNo])
+
+  useEffect(()=>{
+    sessionStorage.setItem('userPhone',userPhone)
+  },[userPhone])
+
+  useEffect(()=>{
+    sessionStorage.setItem('userGroup',userGroup)
+  },[userGroup])
+
+  useEffect(()=>{
+    sessionStorage.setItem('userCenter',userCenter)
+  },[userCenter])
 
   const value = {
-  userName, userType, setUserName, setUserType
+  userNo,
+  userName,
+  userType,
+  userPhone,
+  userGroup,
+  userCenter,
+  setUserNo,
+  setUserName,
+  setUserType,
+  setUserPhone,
+  setUserGroup,
+  setUserCenter,
   }
   return (
     <UserContext.Provider value={value}>
