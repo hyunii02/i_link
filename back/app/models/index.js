0a8c48a6cdadcf74501a753390a31f6cb1f06d28
soaeng <sodud0695@naver.com>
2022-08-09 15:49:49 +0900

    fix:back: DB Timezone 설정

--- back/app/models/index.js
+++ back/app/models/index.js
@@ -8,6 +8,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
+  timezone: dbConfig.timezone,
   operatorsAliases: 0,
 
   pool: {
