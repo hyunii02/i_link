48f0d452111a87df0d657677b4681e369fbcf8d0
배지우 <wldn102@naver.com>
2022-08-12 16:05:20 +0900

    [S07P12E102-171] feat:front:memo_notice

--- front/i_link/src/components/Notice/noticedetail.jsx
+++ front/i_link/src/components/Notice/noticedetail.jsx
@@ -6,35 +6,64 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
 
 const NoticeDetail = (props) => {
   const { detailNotice } = props;
+  
 
   return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { mt: 5 }, // 텍스트필드마다 mt 5
-
-        height: 500,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <Box>
-        <h1>공지사항</h1>
-
-        
+    <Box sx={{ width: 800, height: 1000, background: "#F8FAD7" }}>
+      <Box
+        sx={{
+          background: "#FCEBF6",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          border: "3px solid #DFC5FC",
+          p: 0.5,
+        }}
+      >
+        <Typography
+          id="font_test"
+          fullwidth="true"
+          variant="h5"
+          sx={{
+            height: 50,
+            background: "#FCEBF6",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          {detailNotice.notice_title}
+        </Typography>
       </Box>
+      <Box sx={{ height: 370, width: 790, mt: 10, background: "blue" }}>
+        {/* <TextareaAutosize
+          maxRows={4}
+          defaultValue={detailNotice.notice_content}
+          
+          > */}
 
-      <Typography variant="h5" fontSize="20px" sx={{ height: 50 }}>
-        제목 : {detailNotice.notice_title}
-      </Typography>
-      
-      <Typography>{detailNotice.notice_content}</Typography>
+        <Typography
+          fontSize="15px"
+          id="font_test"
+          minrows={3}
+          fullwidth="true"
+          sx={{
+            height: 310,
+            width: 730,
+            background: "#FCEBF6",
+            border: "3px solid #DFC5FC",
+            p: 4,
+          }}
+        >
+          {detailNotice.notice_content}
+        </Typography>
+        {/* </TextareaAutosize> */}
+      </Box>
     </Box>
   );
 };
