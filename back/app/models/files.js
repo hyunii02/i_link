e5e40c1edeec0ab10205bd0fc5f380652828088b
이소영 <sodud0695@naver.com>
2022-08-17 17:08:39 +0900

    fix:back:  update db-validation

--- back/app/models/files.js
+++ back/app/models/files.js
@@ -1,58 +1,58 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('files', {
-    file_no: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    notice_no: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'notices',
-        key: 'notice_no'
+const Sequelize = require("sequelize");
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    "files",
+    {
+      file_no: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
       },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE',
-    },
-    file_name: {
-      type: DataTypes.STRING(30),
-      allowNull: true
-    },
-    file_size: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    file_type: {
-      type: DataTypes.STRING(500),
-      allowNull: true
-    },
-    file_location: {
-      type: DataTypes.STRING(500),
-      allowNull: true
-    }
-  }, {
-    sequelize,
-    tableName: 'files',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "file_no" },
-        ]
+      notice_no: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "notices",
+          key: "notice_no",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
-      {
-        name: "FILE_NOTICE_FK_idx",
-        using: "BTREE",
-        fields: [
-          { name: "notice_no" },
-        ]
+      file_name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
       },
-    ]
-  });
+      file_size: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      file_type: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      file_location: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      tableName: "files",
+      timestamps: false,
+      indexes: [
+        {
+          name: "PRIMARY",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "file_no" }],
+        },
+        {
+          name: "FILE_NOTICE_FK_idx",
+          using: "BTREE",
+          fields: [{ name: "notice_no" }],
+        },
+      ],
+    },
+  );
 };
--- back/app/models/reports.js
+++ back/app/models/reports.js
@@ -1,85 +1,81 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('reports', {
-    report_no: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
+const Sequelize = require("sequelize");
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    "reports",
+    {
+      report_no: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      kid_no: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "kids",
+          key: "kid_no",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      report_writer: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "user_no",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      report_type: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "report_types",
+          key: "report_type",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      report_content: {
+        type: DataTypes.STRING(500),
+        allowNull: false,
+      },
+      report_date: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
     },
-    kid_no: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'kids',
-        key: 'kid_no'
-      },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE',
+    {
+      sequelize,
+      tableName: "reports",
+      timestamps: false,
+      indexes: [
+        {
+          name: "PRIMARY",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "report_no" }],
+        },
+        {
+          name: "REPORT_KID_FK_idx",
+          using: "BTREE",
+          fields: [{ name: "kid_no" }],
+        },
+        {
+          name: "REPORT_USER_FK_idx",
+          using: "BTREE",
+          fields: [{ name: "report_writer" }],
+        },
+        {
+          name: "REPORT_TYPE_FK_idx",
+          using: "BTREE",
+          fields: [{ name: "report_type" }],
+        },
+      ],
     },
-    report_writer: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'users',
-        key: 'user_no'
-      },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE',
-    },
-    report_type: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      references: {
-        model: 'report_types',
-        key: 'report_type'
-      },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE',
-    },
-    report_content: {
-      type: DataTypes.STRING(500),
-      allowNull: false
-    },
-    report_date: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
-    }
-  }, {
-    sequelize,
-    tableName: 'reports',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "report_no" },
-        ]
-      },
-      {
-        name: "REPORT_KID_FK_idx",
-        using: "BTREE",
-        fields: [
-          { name: "kid_no" },
-        ]
-      },
-      {
-        name: "REPORT_USER_FK_idx",
-        using: "BTREE",
-        fields: [
-          { name: "report_writer" },
-        ]
-      },
-      {
-        name: "REPORT_TYPE_FK_idx",
-        using: "BTREE",
-        fields: [
-          { name: "report_type" },
-        ]
-      },
-    ]
-  });
+  );
 };
